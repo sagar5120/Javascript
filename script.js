e8a7fba9681822c206e65765a748ce069ef16839
[Sagar Bhandari] <[bhandarisagar512@gmail.com]>
2023-09-03 18:47:03 +0545

    Javascript 1st commit

---
+++ script.js
@@ -0,0 +1,65 @@
+/*
+// let
+// let age;
+// age=20;
+//console.log(age);
+
+// var
+//var age;
+//age=20;
+//console.log(age);
+
+//const pi
+//pi = 3.14;
+
+// let name = "sagar";
+
+//Number
+//let age=20;
+//let marks=30;
+//let marksid=20.55;
+
+//string
+//let Name = "Sagar Bhandari";
+console.log(Name);
+
+let car =null;
+car="Bmw";
+console.log(car);
+
+let eng = parseInt (prompt("Enter your marks in English."));
+let nepali =parseInt( prompt("Enter your marks in Nepali."));
+let math = parseInt(prompt("Enter your marks in Soial."));
+let soci = parseInt( prompt("Enter your marks in Social."));
+let mp =parseInt( prompt("Enter your marks in MP."));
+let percentage = (eng+nepali+math+mp+soci)/5;
+console.log(percentage);
+alert(`you got ${percentage}`);
+*/
+/*
+
+
+Write js code to take name of user,his/her faculty name, his/her gpa of 8 semesters(each using prompt) and his/her exam roll number as inputs and then calculate the cgpa of the 8 semesters and display the output as:
+ I am "Name" and I am enrolled in "Faculty name". My exam roll number is "Roll number" and I scored "Cgpa" CGPA in my Bachelor's.*/
+ 
+ /*let Name =  prompt("Enter your name.");
+ let faculty = prompt("Enter your faculty.");
+ let roll = prompt("Enter your roll no.");
+ let one = parseFloat ( prompt("Enter your gpa in 1st sem."));
+ let two = parseFloat ( prompt("Enter your gpa in 2nd sem."));
+let three = parseFloat ( prompt("Enter your gpa in 3rd sem."));
+let four = parseFloat ( prompt("Enter your gpa in 4th sem."));
+let five = parseFloat ( prompt("Enter your gpa in 5th sem."));
+let six = parseFloat ( prompt("Enter your gpa in 6th sem."));
+let seven = parseFloat ( prompt("Enter your gpa in 7th sem."));
+let eight = parseFloat ( prompt("Enter your gpa in 8th sem."));
+ let cgpa = (one+two+three+four+five+six+seven+eight)/8;
+ alert(`I am ${Name} and I am enrolled in ${faculty}. My exam roll number is ${roll} and I scored ${cgpa} CGPA in my Bachelor's. `);
+
+*/
+
+
+
+
+
+
